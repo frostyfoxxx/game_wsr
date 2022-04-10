607d412ca44e0e440df9b3bab8648c533e010d3c
Nail <frosty.fox14@yandex.ru>
2022-04-10 20:33:21 +0400

    Перемещение и проверка выбранной карты

--- js/game.js
+++ js/game.js
@@ -5,11 +5,15 @@ const ctx = canvas.getContext("2d");
 const map1 = document.querySelector("#map1");
 const map2 = document.querySelector("#map2");
 const character = new Image();
-character.src = "img/heroFrames.png";
+let selectedMap = false;
+character.src = "/img/character.png";
 let xPoc = 200;
 let yPoc = 200;
 let wPoc = 80;
 let hPoc = 80;
+let speed = 10;
+let background = new Image();
+background.src = "/img/map.jpg";
 
 const changeMap = (event) => {
   const element = event.target;
@@ -17,83 +21,44 @@ const changeMap = (event) => {
     map2.classList.remove("selected");
     map1.classList.add("selected");
     document.body.className = "map1";
+    selectedMap = true;
   } else {
     map1.classList.remove("selected");
     map2.classList.add("selected");
     document.body.className = "map2";
+    selectedMap = true;
   }
 };
 map1.addEventListener("click", changeMap);
 map2.addEventListener("click", changeMap);
 
 async function game() {
-  if (document.getElementById("nickname").value != "") {
+  document.addEventListener("keydown", control);
+  if (document.getElementById("nickname").value != "" && selectedMap === true) {
     canvas.style.display = "block";
     menu.style.display = "none";
-    start();
+    render();
   } else {
-    alert("Введите ваше имя");
+    alert("Введите ваше имя или выберите карту");
   }
 }
 
-const start = () => {
+function render() {
+  requestAnimationFrame(render);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(character, xPoc, yPoc, wPoc, hPoc);
-  function Movement() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    let hasMoved = false;
-    if (counter % 2 != 0) {
-      if (keyPressed.w || keyPressed.ArrowUp || keyPressed.W) {
-        moveCharacter(0, -PlSpeed, Up);
-        hasMoved = true;
-      } else if (keyPressed.s || keyPressed.ArrowDown || keyPressed.S) {
-        moveCharacter(0, PlSpeed, Down);
-        hasMoved = true;
-      }
-      if (keyPressed.a || keyPressed.ArrowLeft || keyPressed.A) {
-        moveCharacter(-PlSpeed, 0, Left);
-        hasMoved = true;
-      } else if (keyPressed.d || keyPressed.ArrowRight || keyPressed.D) {
-        moveCharacter(PlSpeed, 0, Right);
-        hasMoved = true;
-      }
-      if (hasMoved) {
-        frameCount++;
-        if (frameCount >= PlFrames) {
-          frameCount = 0;
-          currentLoopIndex++;
-          if (currentLoopIndex >= PlFrameCycle.length) {
-            currentLoopIndex = 0;
-          }
-          if (PlFrameCycle[currentLoopIndex] == 3) {
-          } else if (PlFrameCycle[currentLoopIndex] == 5);
-        }
-      }
-      if (!hasMoved) {
-        currentLoopIndex = 0;
-      }
-    }
-    drawFrame(PlFrameCycle[currentLoopIndex], currentDirection, posX, posY);
-    window.requestAnimationFrame(Movement);
-    //
-    update(); //враги
+}
 
-    EnemyCollision(); //враги
-
-    Finish(); //финиш
-    loadMonster(); //враги
-
-    if (map == 1) {
-      //для первой карты
-      Collision();
-      drawFinish(); //финиш
-      FinishCollision(); //финиш
-    } else {
-      //для второй карты
-      Collision2();
-      drawFinish2();
-      FinishCollision2();
-    }
+function control(e) {
+  xPoc = Math.max(0, Math.min(canvas.width - wPoc, xPoc));
+  yPoc = Math.max(0, Math.min(canvas.height - hPoc, yPoc));
+  if (e.keyCode == 37) {
+    xPoc -= speed;
+  } else if (e.keyCode == 38) {
+    yPoc -= speed;
+  } else if (e.keyCode == 39) {
+    xPoc += speed;
+  } else if (e.keyCode == 40) {
+    yPoc += speed;
   }
-};
-
-startBtn.addEventListener("click", game);
+}
